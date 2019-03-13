1f83f9f72aae83b13d54a63cf7282e7880c3c36b
Pallich <palllich@gmail.com>
2019-03-13 21:43:44 +0200

    Add project files.

---
+++ WebDriver_1/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WebDriver_1
+{
+       public class UnitTest1
+
+    {
+        IWebDriver driver;
+
+       
+            [SetUp]
+        public void Setup()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.ukr.net");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            var windowHandle = driver.CurrentWindowHandle;
+
+            IWebElement element = driver.FindElement(By.CssSelector("test css"));
+
+            var elements = driver.FindElements(By.CssSelector("test css"));
+
+            var allWinowhandlers = driver.WindowHandles;
+
+            driver.SwitchTo().Window(windowHandle);
+
+           
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+        }
+    }
+
